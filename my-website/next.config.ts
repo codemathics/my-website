cd78f6af68d5616ff8ca96c53a5c9f7d84fc53ca
codemathics <clemzyport@gmail.com>
2025-09-14 19:30:41 -0700

    added portfolio redirect

--- my-website/next.config.ts
+++ my-website/next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* config options here (leaving this here to remind me of any configurations i need to add) */
+  async redirects() {
+    return [
+      {
+        source: "/portfolio",
+        destination: "https://glowing-bean-c67.notion.site/Clement-Hugbo-a776e38330ad40f684007bd25e97fa4e?source=copy_link",
+        permanent: true, // 308 (use false for 307/temp)
+      },
+      // optional: redirect anything nested under /portfolio/*
+      {
+        source: "/portfolio/:path*",
+        destination: "https://glowing-bean-c67.notion.site/Clement-Hugbo-a776e38330ad40f684007bd25e97fa4e?source=copy_link/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
